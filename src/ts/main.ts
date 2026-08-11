79ddf679bd300fc607b30e6454b4bbc9a08906ce
Matthias Tausch <31157558+BlissCrimson@users.noreply.github.com>
2026-08-11 22:11:34 +0200

    refactor: update project structure and enhance card component functionality

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,10 +1,15 @@
-import "../../src/styles/style.css";
+import "/src/styles/style.scss";
 
 init();
 
 function init() {
   const fieldRef = document.getElementById("field");
   if (fieldRef) {
-    fieldRef;
+    fieldRef.addEventListener("click", (e) => {
+      const card = (e.target as HTMLElement).closest(       ".card",     ) as HTMLButtonElement;
+      if (card) {
+        card.classList.toggle("is-flipped");
+      }
+    });
   }
 }
