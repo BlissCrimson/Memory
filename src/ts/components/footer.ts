f15fd27196c20b39dc67220ec69e7494c3a19129
Matthias Tausch <31157558+BlissCrimson@users.noreply.github.com>
2026-08-18 17:24:36 +0200

    feat: add imprint page and footer component

---
+++ src/ts/components/footer.ts
@@ -0,0 +1,7 @@
+export function createFooter() {
+  let cardRef = document.querySelector("#footer)");
+  if (!cardRef) return;
+  cardRef.innerHTML = `
+        <a class="" href="imprint.html"></a>
+    `;
+}
