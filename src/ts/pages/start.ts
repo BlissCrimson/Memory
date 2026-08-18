728881cf8e82902d412c6ff6698f3f9feb60c8c6
Matthias Tausch <31157558+BlissCrimson@users.noreply.github.com>
2026-08-18 17:26:22 +0200

    feat: update styles and components; add welcome page functionality

--- src/ts/pages/start.ts
+++ src/ts/pages/start.ts
@@ -1,15 +1,24 @@
+import { navigate } from "../../router";
+import { createFooter } from "../components/footer";
+
 export function createWelcomePage() {
   let cardRef = document.querySelector("#app");
   if (!cardRef) return;
+  const BASE_URL = import.meta.env.BASE_URL;
   cardRef.innerHTML = `
     <section class="welcome">
         <span>It's play time.</span>
         <h1>Ready to play?</h1>
+        <button class="button button__entry">
+            <img class="img img__entry img__entry--button" src="${BASE_URL}assets/icons/stadia_controller.svg" alt="controller">
+            <p>Play</p>
+            <img src="${BASE_URL}assets/icons/arrow.svg" alt="arrow">
+        </button>
     </section>
-    <button class="button button__entry">
-        <img src="./public/assets/icons/stadia_controller.svg" alt="controller">
-        <p>Play</p>
-        <img src="./public/assets/icons/arrow.svg" alt="arrow">
-    </button>
+    <img class="img img__entry img__entry--big" src="${BASE_URL}assets/icons/stadia_controller.svg" alt="controller">
+    <footer id="footer"></footer>
   `;
+
+  const playButtonRef = cardRef.querySelector(".button__entry");
+  playButtonRef?.addEventListener("click", () => navigate("/settings"));
 }
