5dd622a87eca6f9e332e0d929072c74d66dc065c
Matthias Tausch <31157558+BlissCrimson@users.noreply.github.com>
2026-08-20 19:04:29 +0200

    feat: enhance layout and styling; wrap start page content in container, update themes and error page styles

--- src/ts/pages/start.ts
+++ src/ts/pages/start.ts
@@ -12,17 +12,19 @@ export function createWelcomePage() {
   if (!cardRef) return createErrorPage();
   const BASE_URL = import.meta.env.BASE_URL;
   cardRef.innerHTML = `
-    <section class="welcome">
-        <span>It's play time.</span>
-        <h1>Ready to play?</h1>
-        <button class="button button__entry">
-            <img class="img img__entry img__entry--button" src="${BASE_URL}assets/icons/stadia_controller.svg" alt="controller">
-            <p>Play</p>
-            <img src="${BASE_URL}assets/icons/arrow.svg" alt="arrow">
-        </button>
-    </section>
-    <img class="img img__entry img__entry--big" src="${BASE_URL}assets/icons/stadia_controller.svg" alt="controller">
-    <footer id="imprint"></footer>
+    <div class="container">
+        <section class="welcome">
+            <span>It's play time.</span>
+            <h1>Ready to play?</h1>
+            <button class="button button__entry">
+                <img class="img img__entry img__entry--button" src="${BASE_URL}assets/icons/stadia_controller.svg" alt="controller">
+                <p>Play</p>
+                <img src="${BASE_URL}assets/icons/arrow.svg" alt="arrow">
+            </button>
+        </section>
+        <img class="img img__entry img__entry--big" src="${BASE_URL}assets/icons/stadia_controller.svg" alt="controller">
+        <footer id="imprint"></footer>
+    </div>
   `;
   createFooter();
   const playButtonRef = cardRef.querySelector(".button__entry");
